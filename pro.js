a56dfdbaaf44f00321c95392380733b172e135e7
moksha-prajapati-73 <mokshaprajapati73@gmail.com>
2025-10-16 12:10:26 +0530

    Delete pro.js

--- pro.js
+++
@@ -1,7 +0,0 @@
-async function getImage() {
-    let res=await fetch("https://dog.ceo/api/breeds/image/random");
-    let data= await res.json();
-    document.getElementById("box").innerHTML=`<img src="${data.message}">`
-    console.log(data)
-
-}
